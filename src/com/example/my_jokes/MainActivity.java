72ab15574ef6e36274fa8748d0a07962be5356ad
BrianMBrannon <brianbrannon@utexas.edu>
2013-12-01 11:29:26 -0600

    Read and write files to a file.

--- src/com/example/my_jokes/MainActivity.java
+++ src/com/example/my_jokes/MainActivity.java
@@ -7,16 +7,19 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	public final static String MY_JOKE = "com.example.myjokes.JOKE";
 	public final static String MY_JOKE_NUMBER = "com.example.myjokes.JOKENUMBER";
+	public final static String MY_FILE = "MyJokes.txt";
 	private Button submitJokeButton;
 	private FileOutputStream fos;
 	private FileInputStream fis;
@@ -26,12 +29,13 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		try {
-			fos = openFileOutput("MyJokes.txt", MODE_APPEND);
+		/*try {
+			fos = openFileOutput("MY_FILE", MODE_APPEND);
+			Toast.makeText(getApplicationContext(), MY_FILE + " opened", Toast.LENGTH_SHORT).show();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 	}
 
 	@Override
@@ -41,16 +45,21 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	/*
+	 * WARNING: getJokeNumber will sometimes cause the application to crash.
+	 */
+	@SuppressWarnings("deprecation")
 	public int getJokeNumber(String myFileName) throws IOException {
 		fis = openFileInput(myFileName);
 		DataInputStream dataIO = new DataInputStream(fis);
 		String line = null;
 		int potentNum = 0;
 		
-		while ((line = dataIO.readLine()) != null) {
-			potentNum = readNumber(line);
+		if(dataIO.readLine() != null) {
+			while ((line = dataIO.readLine()) != null) {
+				potentNum = readNumber(line);
+			}
 		}
-		
 		return potentNum + 1;
 	}
 	
@@ -72,15 +81,19 @@ public class MainActivity extends Activity {
 		
 		return Integer.parseInt(number);
 	}
+	
 	public void sendJoke(View v) throws IOException {
 		EditText mainEditText = (EditText) findViewById(R.id.mainEdit);
 		String joke = mainEditText.getText().toString();
-		int writeTo = getJokeNumber("MyJokes.txt");
+		//int writeTo = getJokeNumber(MY_FILE);
+		//String line = writeTo + ": " + joke;
+		fos = openFileOutput(MY_FILE, MODE_APPEND);
 		fos.write(joke.getBytes());
+		fos.close();
 		
 		Intent intent = new Intent(this, ShowJoke.class);
-		intent.putExtra(MY_JOKE_NUMBER, Integer.toString(writeTo));
-		intent.putExtra(MY_JOKE, joke);
+		//intent.putExtra(MY_JOKE_NUMBER, Integer.toString(writeTo));
+		//intent.putExtra(MY_JOKE, joke);
 		startActivity(intent);
 	}
 }
--- src/com/example/my_jokes/ShowJoke.java
+++ src/com/example/my_jokes/ShowJoke.java
@@ -1,27 +1,59 @@
 package com.example.my_jokes;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ShowJoke extends Activity {
 
+	public final static String MY_FILE = "MyJokes.txt";
 	private TextView jokeTextView;
+	private ListView listView;
+	private ArrayList<String> listItems;
+	private ArrayAdapter adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_show_joke);
 		
-		Intent intent = getIntent();
-		String joke = intent.getStringExtra(MainActivity.MY_JOKE);
-		String jokeNumber = intent.getStringExtra(MainActivity.MY_JOKE_NUMBER);
-		
-		jokeTextView = (TextView) findViewById(R.id.jokeTextView);
-		jokeTextView.setText(jokeNumber + ": " + joke);
-		jokeTextView.setTextSize(32);
+		listView = (ListView) findViewById(R.id.listView);
+		listItems = new ArrayList<String>();
+		try {
+			FileInputStream fis = openFileInput(MY_FILE);
+			DataInputStream dataIO = new DataInputStream(fis);
+			StringBuffer sBuffer = new StringBuffer();
+			String line = null;
+			Intent intent = getIntent();
+			
+			//String joke = intent.getStringExtra(MainActivity.MY_JOKE);
+			//String jokeNumber = intent.getStringExtra(MainActivity.MY_JOKE_NUMBER);
+			
+			jokeTextView = (TextView) findViewById(R.id.jokeTextView);
+			
+			while((line = dataIO.readLine()) != null){
+				listItems.add(line);
+			}
+			
+			adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+			listView.setAdapter(adapter);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
