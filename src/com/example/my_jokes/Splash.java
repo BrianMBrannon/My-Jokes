55ac16a17b156e4559fe915e0c6608ad3d485a8f
BrianMBrannon <brianbrannon@utexas.edu>
2013-12-30 14:55:22 -0600

    Added a type of splash screen

---
+++ src/com/example/my_jokes/Splash.java
@@ -0,0 +1,46 @@
+package com.example.my_jokes;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class Splash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		
+		Thread timeout = new Thread() {
+			public void run() {
+				try {
+					sleep(2500);
+				}
+				catch (InterruptedException e){
+					e.printStackTrace();
+				}
+				finally {
+					Intent openHomeScreen = new Intent("com.example.my_jokes.HOME");
+					startActivity(openHomeScreen);
+				}
+			}
+		};
+		timeout.start();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash, menu);
+		return true;
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+
+}
